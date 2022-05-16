c43c17f92c65e089fb80e09be463f1722ca5af8a
Silverkadiusz55455 <arkadiusz55455@gmail.com>
2022-05-16 15:55:47 +0200

    VOL1

---
+++ src/main/java/com/example/wykopapp/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.wykopapp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register-form";
+    }
+
+    @GetMapping("/login")
+    public String signIn() {
+        return "signup";
+    }
+
+}
---
+++ src/test/java/com/example/wykopapp/WykopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wykopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WykopAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
